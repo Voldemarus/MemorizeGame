48889e08aa94acff68fa484bc11ef5bae206b409
Vladimir Vodolazkiy <vloldmarus@gmail.com>
2023-07-09 11:35:54 +0300

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Vladimir Vodolazkiy on 09.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
